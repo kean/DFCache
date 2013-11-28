c282ec7a4f0dbabc37bcbc2786c476797da585ef
a.grebenyuk <a.grebenyuk@corp.mail.ru>
2013-11-28 14:58:24 +0400

    DFCache + DFStorage

---
+++ DwarfKit/DwarfKit/Tools/Caching/DFStorage.h
@@ -0,0 +1,101 @@
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2013 Alexander Grebenyuk (github.com/kean).
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+static const unsigned long long DFStorageDiskCapacityUnlimited = 0;
+
+/** Key-value disk storage with LRU cleanup (when you need it).
+ */
+NS_CLASS_AVAILABLE(10_7, 5_0)
+@interface DFStorage : NSObject
+
+/** Initializes and returns cache with provided name.
+ @param name Name defines the name of the folder used by storage.
+ */
+- (id)initWithName:(NSString *)name;
+
+/** Returns storage name.
+ */
+@property (nonatomic, readonly) NSString *name;
+
+/** Maximum storage capacity. If 0 then disk space is unlimited.
+ @discussion Not a strict limit. Disk storage is actually cleaned up each time application resigns active (for iOS) and any time - (void)cleanupDisk gets called.
+ */
+@property (nonatomic) unsigned long long diskCapacity;
+
+/** Remaining disk usage after cleanup. The rate must be in the range of 0.0 to 1.0 where 1.0 represents full disk capacity.
+ */
+@property (nonatomic) CGFloat cleanupRate;
+
+#pragma mark - Read
+
+/** Reads data from disk.
+ @param key The unique key.
+ @param completion Completion block.
+ */
+- (void)readDataForKey:(NSString *)key completion:(void (^)(NSData *data))completion;
+
+/** Reads data from disk synchronously.
+ @param key The unique key.
+ */
+- (NSData *)readDataForKey:(NSString *)key;
+
+/** Reads multiple NSData objects for provided keys.
+ @param keys Array of unique keys.
+ @param completion Completion block.
+ */
+- (void)readBatchForKeys:(NSArray *)keys completion:(void (^)(NSDictionary *batch))completion;
+
+/** Returns YES if storage contains data for provided key.
+ */
+- (BOOL)containsDataForKey:(NSString *)key;
+
+#pragma mark - Write
+
+/** Writes data into disk storage asyncronously.
+ @param data Data to store into disk cache.
+ @param key The unique key.
+ */
+- (void)writeData:(NSData *)data forKey:(NSString *)key;
+
+/** Writes data into disk storage synchronously.
+ @param data Data to store into disk cache.
+ @param key The unique key.
+ */
+- (void)writeDataSynchronously:(NSData *)data forKey:(NSString *)key;
+
+/** Writes batch of NSData objects into disk cache.
+ @param batch Dictionary containing { key : data }.
+ @param completion Completion block.
+ */
+- (void)writeBatch:(NSDictionary *)batch;
+
+#pragma mark - Remove
+
+/** Deletes data from storage.
+ */
+- (void)removeDataForKeys:(NSArray *)keys;
+- (void)removeDataForKey:(NSString *)key;
+- (void)removeAllData;
+
+#pragma mark - Maintenance
+
+/** Cleans up disk by removing entries by LRU algorithm.
+ @discussion Cleanup algorithm runs only if max disk cache capacity is set to non-zero value. Calculates target size by multiplying disk capacity and cleanup rate. Files are removed according to LRU algorithm until cache size fits target size.
+ */
+- (void)cleanup;
+
+/** Returns the current size of the receiver’s on-disk cache, in bytes.
+ @warning This operation is expensive.
+ */
+- (unsigned long long)currentDiskUsage;
+
+@end
---
+++ DwarfKit/DwarfKit/Tools/Caching/DFStorage.m
@@ -0,0 +1,304 @@
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2013 Alexander Grebenyuk (github.com/kean).
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "DFCrypto.h"
+#import "DFStorage.h"
+#import "dwarf_private.h"
+
+
+@implementation DFStorage {
+    NSString *_rootPath;
+    dispatch_queue_t _ioQueue;
+    NSCache *_memorizedHashes;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    DWARF_DISPATCH_RELEASE(_ioQueue);
+}
+
+- (id)initWithName:(NSString *)name {
+    if (self = [super init]) {
+        if (!name.length) {
+            [NSException raise:@"DFCache" format:@"Attempting to initialize cache without name"];
+        }
+        _name = name;
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+        _rootPath = [paths[0] stringByAppendingPathComponent:name];
+        [self _createRootFolder];
+        
+        _ioQueue = dispatch_queue_create("_df_storage_io_queue", DISPATCH_QUEUE_SERIAL);
+        
+        _memorizedHashes = [NSCache new];
+        _memorizedHashes.countLimit = 150;
+        
+        NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+        [center addObserver:self selector:@selector(applicationWillResignActive:) name:DFApplicationWillResignActiveNotification object:nil];
+#if TARGET_OS_IPHONE
+        [center addObserver:self selector:@selector(applicationDidReceiveMemoryWarning:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+#endif
+    }
+    return self;
+}
+
+#pragma mark - Read
+
+- (void)readDataForKey:(NSString *)key completion:(void (^)(NSData *))completion {
+    if (!completion) {
+        return;
+    }
+    if (!key) {
+        _dwarf_callback(completion, nil);
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        NSData *data = [self _dataForKey:key];
+        _dwarf_callback(completion, data);
+    });
+}
+
+- (NSData *)readDataForKey:(NSString *)key {
+    if (!key) {
+        return nil;
+    }
+    __block NSData *data;
+    dispatch_sync(_ioQueue, ^{
+        data = [self _dataForKey:key];
+    });
+    return data;
+}
+
+- (void)readBatchForKeys:(NSArray *)keys completion:(void (^)(NSDictionary *))completion {
+    if (!completion) {
+        return;
+    }
+    if (!keys.count) {
+        _dwarf_callback(completion, nil);
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        NSMutableDictionary *batch = [NSMutableDictionary new];
+        for (NSString *key in keys) {
+            NSData *data = [self _dataForKey:key];
+            if (data && _diskCapacity != DFStorageDiskCapacityUnlimited) {
+                [self _touchFileForKey:key];
+            }
+        }
+        _dwarf_callback(completion, batch);
+    });
+}
+
+- (BOOL)containsDataForKey:(NSString *)key {
+    if (!key) {
+        return NO;
+    }
+    return [self _fileExistsForKey:key];
+}
+
+#pragma mark - Write
+
+- (void)writeData:(NSData *)data forKey:(NSString *)key {
+    if (!data || !key) {
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        [self _writeData:data forKey:key];
+    });
+}
+
+- (void)writeDataSynchronously:(NSData *)data forKey:(NSString *)key {
+    if (!data || !key) {
+        return;
+    }
+    dispatch_sync(_ioQueue, ^{
+        [self _writeData:data forKey:key];
+    });
+}
+
+- (void)writeBatch:(NSDictionary *)batch {
+    if (!batch.count) {
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        [batch enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            [self _writeData:obj forKey:key];
+        }];
+    });
+}
+
+#pragma mark - Remove
+
+- (void)removeDataForKeys:(NSArray *)keys {
+    if (!keys) {
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        for (NSString *key in keys) {
+            [self _deleteDataForKey:key];
+        }
+    });
+}
+
+- (void)removeDataForKey:(NSString *)key {
+    if (key) {
+        [self removeDataForKeys:@[key]];
+    }}
+
+- (void)removeAllData {
+    dispatch_async(_ioQueue, ^{
+        [[NSFileManager defaultManager] removeItemAtPath:_rootPath error:nil];
+        [self _createRootFolder];
+    });
+}
+
+#pragma mark - Disk I/O
+
+- (void)_createRootFolder {
+    NSFileManager *manager = [NSFileManager defaultManager];
+    if (![manager fileExistsAtPath:_rootPath]) {
+        [manager createDirectoryAtPath:_rootPath withIntermediateDirectories:YES attributes:nil error:NULL];
+    }
+}
+
+- (NSString *)_hashWithKey:(NSString *)key {
+    NSString *hash = [_memorizedHashes objectForKey:key];
+    if (!hash) {
+        hash = [DFCrypto MD5FromString:key];
+        [_memorizedHashes setObject:hash forKey:key];
+    }
+    return hash;
+}
+
+- (NSString *)_pathWithKey:(NSString *)key {
+    if (!key) {
+        return nil;
+    }
+    NSString *hash = [self _hashWithKey:key];
+    return [_rootPath stringByAppendingPathComponent:hash];
+}
+
+- (NSData *)_dataForKey:(NSString *)key {
+    NSString *filepath = [self _pathWithKey:key];
+    return [NSData dataWithContentsOfFile:filepath options:NSDataReadingUncached error:nil];
+}
+
+- (void)_touchFileForKey:(NSString *)key {
+    NSString *filepath = [self _pathWithKey:key];
+    NSURL *url = [NSURL fileURLWithPath:filepath];
+    [url setResourceValue:[NSDate date] forKey:NSURLAttributeModificationDateKey error:nil];
+}
+
+- (void)_writeData:(NSData *)data forKey:(NSString *)key {
+    NSString *filepath = [self _pathWithKey:key];
+    NSFileManager *manager = [NSFileManager defaultManager];
+    [manager createFileAtPath:filepath contents:data attributes:nil];
+}
+
+- (BOOL)_fileExistsForKey:(NSString *)key {
+    NSString *filepath = [self _pathWithKey:key];
+    return [[NSFileManager defaultManager] fileExistsAtPath:filepath];
+}
+
+- (void)_deleteDataForKey:(NSString *)key {
+    NSString *filepath = [self _pathWithKey:key];
+    [[NSFileManager defaultManager] removeItemAtPath:filepath error:nil];
+}
+
+#pragma mark - Maintenance
+
+- (void)cleanup {
+    if (_diskCapacity == 0) {
+        return;
+    }
+    dispatch_async(_ioQueue, ^{
+        [self _cleanup];
+    });
+}
+
+- (void)_cleanup {
+    NSFileManager *manager = [NSFileManager defaultManager];
+    NSURL *entriesURL = [NSURL fileURLWithPath:_rootPath isDirectory:YES];
+    NSArray *resourceKeys = @[ NSURLIsDirectoryKey,
+                               NSURLContentModificationDateKey,
+                               NSURLFileAllocatedSizeKey ];
+    NSDirectoryEnumerator *fileEnumerator =
+    [manager enumeratorAtURL:entriesURL
+  includingPropertiesForKeys:resourceKeys
+                     options:NSDirectoryEnumerationSkipsHiddenFiles
+                errorHandler:NULL];
+    
+    NSMutableDictionary *files = [NSMutableDictionary dictionary];
+    _dwarf_bytes currentSize = 0;
+    for (NSURL *fileURL in fileEnumerator) {
+        NSDictionary *resourceValues = [fileURL resourceValuesForKeys:resourceKeys error:NULL];
+        if ([resourceValues[NSURLIsDirectoryKey] boolValue]) {
+            continue;
+        }
+        NSNumber *fileSize = resourceValues[NSURLFileAllocatedSizeKey];
+        currentSize += [fileSize unsignedLongLongValue];
+        files[fileURL] = resourceValues;
+    }
+    if (currentSize > _diskCapacity) {
+        const _dwarf_bytes desiredSize = _diskCapacity * _cleanupRate;
+        NSArray *sortedFiles =
+        [files keysSortedByValueWithOptions:NSSortConcurrent usingComparator:^NSComparisonResult(id obj1, id obj2) {
+            return [obj1[NSURLContentModificationDateKey] compare:
+                    obj2[NSURLContentModificationDateKey]];
+        }];
+        for (NSURL *fileURL in sortedFiles) {
+            if ([manager removeItemAtURL:fileURL error:nil]) {
+                NSDictionary *resourceValues = files[fileURL];
+                NSNumber *fileSize = resourceValues[NSURLFileAllocatedSizeKey];
+                currentSize -= [fileSize unsignedLongLongValue];
+                if (currentSize < desiredSize) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+- (_dwarf_bytes)currentDiskUsage {
+    __block _dwarf_bytes size = 0;
+    dispatch_sync(_ioQueue, ^{
+        NSFileManager *manager = [NSFileManager defaultManager];
+        NSURL *entriesURL = [NSURL fileURLWithPath:_rootPath isDirectory:YES];
+        NSArray *resourceKeys = @[ NSURLIsDirectoryKey,
+                                   NSURLFileAllocatedSizeKey ];
+        NSDirectoryEnumerator *fileEnumerator =
+        [manager enumeratorAtURL:entriesURL
+      includingPropertiesForKeys:resourceKeys
+                         options:NSDirectoryEnumerationSkipsHiddenFiles
+                    errorHandler:NULL];
+        for (NSURL *fileURL in fileEnumerator) {
+            NSDictionary *resourceValues = [fileURL resourceValuesForKeys:resourceKeys error:NULL];
+            if ([resourceValues[NSURLIsDirectoryKey] boolValue]) {
+                continue;
+            }
+            size += [resourceValues[NSURLFileAllocatedSizeKey] unsignedLongLongValue];
+        }
+    });
+    return size;
+}
+
+#pragma mark - Application Notifications
+
+- (void)applicationWillResignActive:(NSNotification *)notification {
+    [self cleanup];
+}
+
+- (void)applicationDidReceiveMemoryWarning:(NSNotification *)notification {
+    [_memorizedHashes removeAllObjects];
+}
+
+@end
